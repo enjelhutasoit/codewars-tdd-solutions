a443601ebcbe353a40e9d8a2ff0b2b00120c28d0
Enjel Hutasoit <enjel.hutasoit@gmail.com>
2026-03-06 23:47:11 +0700

    Add ReversedSequence implementation with tests for generating a reversed sequence of integers

--- CodeWarsTest/Easy/ReversedSequence.swift
+++ CodeWarsTest/Easy/ReversedSequence.swift
@@ -3,3 +3,17 @@
 //
 
 
+struct ReversedSequence {
+    /// Problem: Reversed Sequence
+    ///
+    /// Build a function that returns an array of integers from n to 1 where n>0.
+    ///
+    /// Example : n=5 --> [5,4,3,2,1]
+    ///
+    /// Source: https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/swift
+
+    func reverseSeq(n: Int) -> [Int] {
+        guard n > 0 else { return [] }
+        return Array(1...n).reversed()
+    }
+}
--- CodeWarsTests/Easy/ReversedSequenceTests.swift
+++ CodeWarsTests/Easy/ReversedSequenceTests.swift
@@ -5,8 +5,45 @@
 import XCTest
 
 final class ReversedSequenceTests: XCTestCase {
-
-    func testSetup() {
-        XCTAssertTrue(true, "Test file is ready!")
+    
+    private var sut: ReversedSequence!
+    
+    override func setUp() {
+        super.setUp()
+        sut = ReversedSequence()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_reverseSeq_whenNegative_returnsEmptyArray() {
+        XCTAssertEqual(sut.reverseSeq(n: -10), [])
+    }
+    
+    func test_reverseSeq_whenZero_returnsEmptyArray() {
+        XCTAssertEqual(sut.reverseSeq(n: 0), [])
+    }
+    
+    func test_reverseSeq_whenONe_returnsSingleElement() {
+        XCTAssertEqual(sut.reverseSeq(n: 1), [1])
+    }
+    
+    func test_reverseSeq_whenTwo_returnsCorrectSequence() {
+        XCTAssertEqual(sut.reverseSeq(n: 2), [2, 1])
+    }
+    
+    func test_reverseSeq_whenFive_returnsCorrectSequence() {
+        XCTAssertEqual(sut.reverseSeq(n: 5), [5, 4, 3, 2, 1])
+    }
+    
+    func test_reverseSeq_whenLargeInput_returnsCorrectCountAndBounds() {
+        let n = 10_000
+        let result = sut.reverseSeq(n: n)
+        
+        XCTAssertEqual(result.count, n)
+        XCTAssertEqual(result.first, n)
+        XCTAssertEqual(result.last, 1)
     }
 }
