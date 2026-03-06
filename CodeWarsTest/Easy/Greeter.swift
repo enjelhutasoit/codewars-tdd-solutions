f1990d00fa90496e2aade179f9c5400a96b6d5ca
Enjel Hutasoit <enjel.hutasoit@gmail.com>
2026-03-06 23:46:05 +0700

    Add Greeter implementation with greeting function and corresponding tests

--- CodeWarsTest/Easy/Greeter.swift
+++ CodeWarsTest/Easy/Greeter.swift
@@ -2,4 +2,18 @@
 // Copyright © 2026 Enjel Hutasoit. All rights reserved.
 //
 
+struct Greeter {
+    /// Problem: Returning Strings
+    ///
+    /// Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
 
+    /// [Make sure you type the exact thing I wrote or the program may not execute properly]
+    /// Your goal is to write a function that removes the first and last characters of a string. You're given one parameter, the original string.
+    ///
+    /// Source:
+    /// https://www.codewars.com/kata/55a70521798b14d4750000a4/train/swift
+
+    func greet(_ name: String) -> String {
+        "Hello, \(name) how are you doing today?"
+    }
+}
--- CodeWarsTests/Easy/GreeterTests.swift
+++ CodeWarsTests/Easy/GreeterTests.swift
@@ -5,8 +5,20 @@
 import XCTest
 
 final class GreeterTests: XCTestCase {
-
-    func testSetup() {
-        XCTAssertTrue(true, "Test file is ready!")
+    
+    private var sut: Greeter!
+    
+    override func setUp() {
+        super.setUp()
+        sut = Greeter()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_greet_whenNameAsInput_returnsFormattedGreetingWithName() {
+        XCTAssertEqual(sut.greet("Blazej"), "Hello, Blazej how are you doing today?")
     }
 }
