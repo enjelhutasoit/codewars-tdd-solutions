17094ab8a5f22b7bf505f5dcccab819255b06d9b
Enjel Hutasoit <enjel.hutasoit@gmail.com>
2026-03-06 23:45:36 +0700

    Add Clock implementation and corresponding tests for time calculation in milliseconds

--- CodeWarsTest/Easy/Clock.swift
+++ CodeWarsTest/Easy/Clock.swift
@@ -3,3 +3,19 @@
 //
 
 
+struct Clock {
+    /// Problem: Beginner Series #2 Clock
+    ///
+    /// Clock shows h hours, m minutes and s seconds after midnight.
+    /// Your task is to write a function which returns the time since midnight in milliseconds.
+    ///
+    /// Example:
+    /// h = 0, m = 1, s = 1 → 61000
+    ///
+    /// Source:
+    /// https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
+
+    func past(_ h: Int, _ m: Int, _ s: Int) -> Int {
+        ((h * 3600) + (m * 60) + s) * 1000
+    }
+}
--- CodeWarsTests/Easy/ClockTests.swift
+++ CodeWarsTests/Easy/ClockTests.swift
@@ -5,8 +5,36 @@
 import XCTest
 
 final class ClockTests: XCTestCase {
-
-    func testSetup() {
-        XCTAssertTrue(true, "Test file is ready!")
+    
+    private var sut: Clock!
+    
+    override func setUp() {
+        super.setUp()
+        sut = Clock()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_past_whenTimeIsZero_returnsZeroMilliseconds() {
+        XCTAssertEqual(sut.past(0, 0, 0), 0)
+    }
+    
+    func test_past_whenTimeIsOneMinuteOneSecond_returns61000Milliseconds() {
+        XCTAssertEqual(sut.past(0, 1, 1), 61000)
+    }
+    
+    func test_past_whenTimeIsOneHourOneMinuteOneSecond_returns61000Milliseconds() {
+        XCTAssertEqual(sut.past(1, 1, 1), 3661000)
+    }
+    
+    func test_past_whenTimeIsOneHourOneSecond_returns61000Milliseconds() {
+        XCTAssertEqual(sut.past(1, 0, 1), 3601000)
+    }
+    
+    func test_past_whenTimeIsOneHour_returns3600000Milliseconds() {
+        XCTAssertEqual(sut.past(1, 0, 0), 3600000)
     }
 }
