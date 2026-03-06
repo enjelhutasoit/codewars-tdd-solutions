22ece8985504fd79b8f89475d35fea5dc2f02013
Enjel Hutasoit <enjel.hutasoit@gmail.com>
2026-03-06 23:44:31 +0700

    Add CenturyCalculator implementation to calculate the century from a given year

--- CodeWarsTest/Easy/CenturyCalculator.swift
+++ CodeWarsTest/Easy/CenturyCalculator.swift
@@ -3,3 +3,25 @@
 //
 
 
+struct CenturyCalculator {
+    /// Problem: Century From Year
+    ///
+    /// The first century spans from the year 1 up to and including the year 100,
+    /// the second century from the year 101 up to and including the year 200, etc. :contentReference[oaicite:0]{index=0}
+    ///
+    /// Task:
+    /// Given a year, return the century it belongs to. :contentReference[oaicite:1]{index=1}
+    ///
+    /// Examples:
+    /// 1705 → 18
+    /// 1900 → 19
+    /// 1601 → 17
+    /// 2000 → 20
+    ///
+    /// Source:
+    /// https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+    
+    func century(_ year: Int) -> Int {
+        (year + 99) / 100
+    }
+}
--- CodeWarsTests/Easy/CenturyCalculatorTests.swift
+++ CodeWarsTests/Easy/CenturyCalculatorTests.swift
@@ -5,8 +5,36 @@
 import XCTest
 
 final class CenturyCalculatorTests: XCTestCase {
-
-    func testSetup() {
-        XCTAssertTrue(true, "Test file is ready!")
+    
+    private var sut: CenturyCalculator!
+    
+    override func setUp() {
+        super.setUp()
+        sut = CenturyCalculator()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_century_whenYearIs1705_return18() {
+        XCTAssertEqual(sut.century(1705), 18)
+    }
+    
+    func test_century_whenYearIs1900_return19() {
+        XCTAssertEqual(sut.century(1900), 19)
+    }
+    
+    func test_century_whenYearIs1601_return17() {
+        XCTAssertEqual(sut.century(1601), 17)
+    }
+    
+    func test_century_whenYearIs2000_return20() {
+        XCTAssertEqual(sut.century(2000), 20)
+    }
+    
+    func test_century_whenYearIs2742_return28() {
+        XCTAssertEqual(sut.century(2742), 28)
     }
 }
